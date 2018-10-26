27c71671210de7df5409545365a698e891c13426
Zhenya Shepel <shepelzz@gmail.com>
2018-10-26 16:23:09 +0300

    servlet-2

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,23 +1,50 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/test")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Test!");
+        resp.getWriter().println(req.getParameter("param"));
     }
 
     @Override
-    public void destroy() {
-        super.destroy();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //download app POSTMAN
+
+//        req.getInputStream();
+//        req.getReader();
+
+        super.doPost(req, resp);
     }
 
     @Override
-    public void init() throws ServletException {
-        super.init();
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+
+    //1. servlet registration - init()
+    //2. servlet works with service method
+    //3. to close servlet with its resources - destroy
+
+    //HTTP REQUESTS
+    //  GET - get some info
+    //  POST - save some info
+    //  PUT - update
+    //  DELETE - delete info
+
+
+
+
+
 }
